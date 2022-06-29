78377321d5ccadb04bd24af0bfee21e8dbef32b4
Hameedullah Khan <hameedullah.khan@bluerivertech.com>
2022-06-29 18:29:09 +0400

    Add the AWS access keys

--- vars/awsProfileConfig.groovy
+++ vars/awsProfileConfig.groovy
@@ -1,6 +1,9 @@
 def call(def aws_access_key_id, def aws_secret_key_id, def aws_region) {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
-    sh "echo ${aws_access_key_id}"
+    AWS_ACCESS_KEY = credentials aws_access_key_id
+    AWS_SECRET_KEY = credentials aws_secret_key_id
+    AWS_REGION = aws_region
+    sh "echo ${aws_access_key_id}
 
 }
