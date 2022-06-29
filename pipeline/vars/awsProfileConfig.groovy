1ba5334ed99bbddbde1dd0f4ed0c3657deaebd35
Hameedullah Khan <hameedullah.khan@bluerivertech.com>
2022-06-29 20:35:17 +0400

    Move pipeline to the subfolder

--- pipeline/vars/awsProfileConfig.groovy
+++ pipeline/vars/awsProfileConfig.groovy
@@ -1,4 +1,5 @@
 def call(def AWS_ACCESS_KEY, def AWS_SECRET_KEY, def AWS_REGION) {
+    sh "echo Test from pipeline folder"
     sh "aws configure set aws_access_key_id ${AWS_ACCESS_KEY} \
        && aws configure set aws_secret_access_key ${AWS_SECRET_KEY} \
        && aws configure set default.region ${AWS_REGION}"
