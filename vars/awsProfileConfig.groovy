d29180c6b525661e01aea438c39090645f8085bf
Hameedullah Khan <hameedullah.khan@bluerivertech.com>
2022-06-29 17:03:07 +0400

    Simple sh script to test

--- vars/awsProfileConfig.groovy
+++ vars/awsProfileConfig.groovy
@@ -1,6 +1,6 @@
 def call(def aws_access_key_id, def aws_secret_key_id, def aws_region) {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
-    sh "echo ${aws_access_key_id}
+    sh "echo ${aws_access_key_id}"
 
 }
