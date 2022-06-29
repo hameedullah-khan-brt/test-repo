76d892823f741aa88d2e0dd9d15af878ebebe7c8
Hameedullah Khan <hameedullah.khan@bluerivertech.com>
2022-06-29 18:34:26 +0400

    Fix the script

--- vars/awsProfileConfig.groovy
+++ vars/awsProfileConfig.groovy
@@ -4,6 +4,6 @@ def call(def aws_access_key_id, def aws_secret_key_id, def aws_region) {
     AWS_ACCESS_KEY = credentials aws_access_key_id
     AWS_SECRET_KEY = credentials aws_secret_key_id
     AWS_REGION = aws_region
-    sh "echo ${aws_access_key_id}
+    sh "echo ${aws_access_key_id}"
 
 }
