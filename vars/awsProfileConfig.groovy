74e28f32f710f662487a9bfe6d9e48e78fa1311e
Hameedullah Khan <hameedullah.khan@bluerivertech.com>
2022-06-29 11:22:51 +0400

    Update credentials call

--- vars/awsProfileConfig.groovy
+++ vars/awsProfileConfig.groovy
@@ -1,8 +1,8 @@
 def call(def aws_access_key_id, def aws_secret_key_id, def aws_region) {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
-    AWS_ACCESS_KEY = credentials(aws_access_key_id)
-    AWS_SECRET_KEY = credentials(aws_secret_key_id)
+    AWS_ACCESS_KEY = credentials aws_access_key_id
+    AWS_SECRET_KEY = credentials aws_secret_key_id
     AWS_REGION = aws_region
     sh "aws configure set aws_access_key_id ${AWS_ACCESS_KEY} \
         && aws configure set aws_secret_access_key ${AWS_SECRET_KEY} \
