86897b0ce91b921bf9abb41b68908886010b90ae
Hameedullah Khan <hameedullah.khan@bluerivertech.com>
2022-06-29 19:37:00 +0400

    Break sh script

--- vars/awsProfileConfig.groovy
+++ vars/awsProfileConfig.groovy
@@ -4,8 +4,8 @@ def call(def aws_access_key_id, def aws_secret_key_id, def aws_region) {
     def AWS_ACCESS_KEY = credentials aws_access_key_id
     def AWS_SECRET_KEY = credentials aws_secret_key_id
     def AWS_REGION = aws_region
-    sh "aws configure set aws_access_key_id ${AWS_ACCESS_KEY} \
-        && aws configure set aws_secret_access_key ${AWS_SECRET_KEY} \
-        && aws configure set default.region ${AWS_REGION}"
+    sh "aws configure set aws_access_key_id ${AWS_ACCESS_KEY}"
+    sh "aws configure set aws_secret_access_key ${AWS_SECRET_KEY}"
+    sh "aws configure set default.region ${AWS_REGION}"
 
 }
